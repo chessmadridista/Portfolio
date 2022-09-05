950972c7aafb405a41f7fee65435b592d674d224
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-06 03:39:01 +0530

    Add blueprint for the timeline component.

---
+++ app/src/components/ProfessionalSoftwareDevelopment/WorkHistory.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-timeline>
+        <v-timeline-item>
+            <p>Para lol</p>
+        </v-timeline-item>
+    </v-timeline>
+</template>
+<script>
+export default {
+    
+}
+</script>
