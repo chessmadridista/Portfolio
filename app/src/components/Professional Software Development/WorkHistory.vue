6a43f17c6ec19ffde5b4814d720b73f0a0bcd3e6
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-10 22:05:16 +0530

    Remove unnecessary components. Import the json file via named importing method.
    Using wildcard import adds a bug to the app which makes the component which shows the data related to the json, twice.

---
+++ app/src/components/Professional Software Development/WorkHistory.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <!-- TODO: Add the project tech stack used by separating them using bullet points in the same line. -->
+        <p
+        v-for="experience of pastExperience"
+        :key="experience.experienceID">ID: {{ experience.experienceID }}</p>
+    </v-container>
+</template>
+<script>
+import pastExperience from '../../assets/Professional Software Development/pastExperience.json';
+
+export default {
+    name: "WorkHistory",
+    data() {
+        return {
+            pastExperience: pastExperience,
+        };
+    },
+};
+</script>
