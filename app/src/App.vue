10798cc6308dba117c379ceb43823982195ab057
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-12 00:14:59 +0530

    Add loader and store.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <Loader />
+    <router-view />
+  </v-app>
 </template>
+<script>
+import Loader from './components/Auxiliary/Loader.vue';
+
+export default {
+  name: "App",
+  components: {
+    Loader,
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    endLoading() {
+      setTimeout(() => {
+        this.$store.dispatch("endLoading")
+        .then(() => {
+        });
+      }, 1500);
+
+      return true;
+    },
+  },
+  watch: {
+    $route() {
+      this.$store.dispatch("startLoading");
+      this.endLoading();
+    },
+  },
+  mounted() {
+    this.endLoading();
+  },
+};
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ app/src/components/Auxiliary/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-overlay
+    opacity="0.9"
+    :value="this.$store.state.isLoading"
+    >  
+      <v-progress-circular
+      indeterminate
+      color="primary"
+      >
+      </v-progress-circular>
+    </v-overlay>
+</template>
+<script>
+export default {
+    name: 'Loader',
+}
+</script>
