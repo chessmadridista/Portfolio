de7624dd45d0ee7e407f79783320c933af4c4fb0
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-11 22:50:51 +0530

    Add all the relevant info from your resume.

--- app/src/components/Professional Software Development/WorkHistory.vue
+++ app/src/components/Professional Software Development/WorkHistory.vue
@@ -1,9 +1,39 @@
 <template>
     <v-container>
         <!-- TODO: Add the project tech stack used by separating them using bullet points in the same line. -->
-        <p
+        <div 
         v-for="experience of pastExperience"
-        :key="experience.experienceID">ID: {{ experience.experienceID }}</p>
+        :key="experience.experienceID"
+        >
+            <h3>{{ experience.role }} at {{ experience.company }}</h3>
+            <p>{{ experience.startDate }} - {{ experience.endDate }}</p>
+            <v-timeline>
+                <v-timeline-item
+                color="#00acee"    
+                v-for="project of experience.projects"
+                :key="project.projectID"
+                >
+                    <h4>{{ project.projectName }}</h4>
+                    <ul>
+                        <li
+                        v-for="responsibility of project.projectResponsibility"
+                        :key="responsibility.projectRespID"
+                        >
+                            <p>{{ responsibility.projectRespDescription }}</p>
+                        </li>
+                    </ul>
+                    <div v-if="isTechStackUsed(project.projectTechStack)">
+                        <h5>Tech stack used:</h5>
+                        <v-chip
+                        v-for="techStack of project.projectTechStack"
+                        :key="techStack"
+                        >
+                        {{ techStack }}
+                        </v-chip>
+                    </div>
+                </v-timeline-item>
+            </v-timeline>       
+        </div>
     </v-container>
 </template>
 <script>
@@ -15,6 +45,11 @@ export default {
         return {
             pastExperience: pastExperience,
         };
+    },
+    methods: {
+        isTechStackUsed(techStack) {
+            return techStack.length > 0;
+        },
     },
 };
 </script>
