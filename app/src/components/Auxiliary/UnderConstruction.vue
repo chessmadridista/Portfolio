6d3efff89d33491a8a4bdeb794218bbaaf7dacd6
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-14 23:32:46 +0530

    Add error message component in case a page is unavailable.

--- app/src/components/Auxiliary/UnderConstruction.vue
+++ app/src/components/Auxiliary/UnderConstruction.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
-        <img src="../../assets/images/Under Construction.jpg" alt="">
-    </div>
+    <v-container>
+        <div class="image__container">
+            <img src="../../assets/images/Under Construction.jpg" alt="Under construction">
+        </div>
+        <div class="image__dimmer"></div>
+        <v-card class="pa-10 elevation-0">
+            <v-card-title class="justify-center">
+                ERROR 503
+            </v-card-title>
+            <v-card-text class="text-center">
+                Oops, it seems that this page is currently under development. Let's hope that
+                it gets ready by the next time you check it out :&#41;!
+            </v-card-text>
+            <p></p>
+        </v-card>
+    </v-container>
 </template>
 <script>
 export default {
@@ -9,7 +22,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-div {
+.image__container {
     position: fixed;
     top: 0;
     left: 0;
@@ -17,6 +30,26 @@ div {
     img {
         max-height: 100%;
         max-width: 100%;
+    }
+}
+.image__dimmer {
+    position:fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(rgba(255, 167, 167, 0.3), rgba(0, 0, 0, 0.8));
+}
+.v-card {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    height: 220px;
+    width: 600px;
+
+    .v-card__text {
+        color: #000;
     }
 }
 </style>
