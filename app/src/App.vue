c90812e8d46b6ca0038d94e3d85e1eb79db11f54
Lavanya Mishra <lavanyamishra1996@gmail.com>
2022-09-18 01:20:26 +0530

    Add box-shadow to scroll bar.

--- app/src/App.vue
+++ app/src/App.vue
@@ -56,6 +56,7 @@ main {
 ::-webkit-scrollbar-thumb {
     border-radius: 10px;
     background: rgb(171, 171, 255);
+    box-shadow: inset 0 0 6px rgba(0,0,0,0.5);
     -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
 }
 #app {
