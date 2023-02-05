5c64973751690d5319eba087ae5650c51598b9a3
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-02-05 16:56:12 +0530

    Add form fields.

--- app/src/components/about/About.vue
+++ app/src/components/about/About.vue
@@ -3,13 +3,32 @@
         <v-card class="pa-6">
             <v-card-title class="blue--text justify-center">About Me</v-card-title>
             <v-card-text>
-                <p>Hi, I am Lavanya and my main passion is music but I also like to work on software development projects and I also write articles on the side as well. Feel free to send me a note using the textbox below :&#41;.</p>
-                <v-form>
-                    <v-textarea 
-                    outlined 
-                    label="Message"
-                    placeholder="Hello, I would like to collaborate on the background music theme for my play..."
-                    ></v-textarea>
+                <p>Hi, I am Lavanya and my main passion is music but I also like to work on software development projects and I also write articles on the side as well. Feel free to send me a note using the form below :&#41;.</p>
+                <v-form class="mt-10">
+                    <v-row>
+                        <v-col md="6">
+                            <v-text-field outlined label="First name"></v-text-field>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field outlined label="Last name"></v-text-field>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field outlined label="Email"></v-text-field>
+                        </v-col>
+                        <v-col md="6">
+                            <v-text-field outlined label="Phone"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <v-textarea 
+                            outlined 
+                            label="Message*"
+                            placeholder="Hello, I would like to collaborate on the background music theme for my play..."
+                            required
+                            ></v-textarea>
+                        </v-col>
+                    </v-row>
                     <v-row>
                         <v-col>
                             <v-btn text color="error">Reset</v-btn>
