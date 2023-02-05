54b5c0888d59cdfaf24589d008725a7bf73d6a65
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-02-05 15:59:32 +0530

    Fill up the template of the 'About' page.

--- app/src/components/about/About.vue
+++ app/src/components/about/About.vue
@@ -1,14 +1,31 @@
 <template>
-    <UnderConstruction />
+    <v-container>
+        <v-card class="pa-6">
+            <v-card-title class="blue--text justify-center">About Me</v-card-title>
+            <v-card-text>
+                <p>Hi, I am Lavanya and my main passion is music but I also like to work on software development projects and I also write articles on the side as well. Feel free to send me a note using the textbox below :&#41;.</p>
+                <v-form>
+                    <v-textarea 
+                    outlined 
+                    label="Message"
+                    placeholder="Hello, I would like to collaborate on the background music theme for my play..."
+                    ></v-textarea>
+                    <v-row>
+                        <v-col>
+                            <v-btn text color="error">Reset</v-btn>
+                        </v-col>
+                        <v-col class="text-right">
+                            <v-btn color="primary" class="text-right">Send</v-btn>
+                        </v-col>
+                    </v-row>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
 <script>
-import UnderConstruction from '../auxiliary/UnderConstruction.vue';
-
 export default {
     name: "About",
-    components: {
-        UnderConstruction,
-    },
     data() {
         return {
             pageTitle: "About me",
