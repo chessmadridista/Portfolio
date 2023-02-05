2afe2a41432b1a1e04283ba8deda85bd8f7dd504
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-02-05 17:04:33 +0530

    Add type of the form fields.

--- app/src/components/about/About.vue
+++ app/src/components/about/About.vue
@@ -1,30 +1,32 @@
 <template>
     <v-container>
-        <v-card class="pa-6">
+        <v-card class="pa-6 mb-8">
             <v-card-title class="blue--text justify-center">About Me</v-card-title>
             <v-card-text>
                 <p>Hi, I am Lavanya and my main passion is music but I also like to work on software development projects and I also write articles on the side as well. Feel free to send me a note using the form below :&#41;.</p>
                 <v-form class="mt-10">
                     <v-row>
                         <v-col md="6">
-                            <v-text-field outlined label="First name"></v-text-field>
+                            <v-text-field outlined label="First name" type="text"></v-text-field>
                         </v-col>
                         <v-col md="6">
-                            <v-text-field outlined label="Last name"></v-text-field>
+                            <v-text-field outlined label="Last name" type="text"></v-text-field>
                         </v-col>
                         <v-col md="6">
-                            <v-text-field outlined label="Email"></v-text-field>
+                            <v-text-field outlined label="Email" type="email"></v-text-field>
                         </v-col>
                         <v-col md="6">
-                            <v-text-field outlined label="Phone"></v-text-field>
+                            <v-text-field outlined label="Phone" type="number"></v-text-field>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col>
                             <v-textarea 
+                            counter="500"
                             outlined 
                             label="Message*"
                             placeholder="Hello, I would like to collaborate on the background music theme for my play..."
+                            max="20"
                             required
                             ></v-textarea>
                         </v-col>
